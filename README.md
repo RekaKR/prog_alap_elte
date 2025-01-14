69068c2d912c0f5662d8a84771c416168017a170
Réka Kupsza <rkupsza@diligent.com>
2025-01-14 14:39:11 +0100

    add Program.cs

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# prog_alap_elte
+# prog alap elte
+
+## Kód futtatás:
+
+### VSC:
+
+- Opciónális: Telepitsd VSC-ben az extensions résznél a C#-ot a Microsoft-tól.
+- Lépj be az algo_kod mappába.
+- Az adott kódrészt kommenteld ki (szedd ki előle a /\* és mögüle a \_/-t).
+- A terminálba ird be, hogy: `dotnet run` és nyomj egy entert.
---
+++ algo_kod/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        // összegzés
+        int n = 2;
+        int[] tomb = {1, 2};
+        
+        int osszeg = 0;
+        for (int i = 0; i < n; i++) {
+            osszeg += tomb[i];
+        }
+
+        Console.WriteLine(osszeg);
+        */
+
+        /*
+        // feltételes összegzés
+        int n = 2;
+        int[] tomb = {14, 2};
+        
+        int osszeg = 0;
+        for (int i = 0; i < n; i++) {
+            if (tomb[i] > 10) {
+                osszeg += tomb[i];
+            }
+        }
+
+        Console.WriteLine(osszeg);
+        */
+
+        /*
+        // megszámlálás
+        int n = 2;
+        int[] tomb = { 14, 2 };
+        
+        int db = 0;
+        for (int i = 0; i < n; i++) {
+            if (tomb[i] > 10) {
+                db++;
+            }
+        }
+
+        Console.WriteLine(db);
+        */
+
+        /*
+        // maximum kiválasztás
+        int n = 3;
+        int[] tomb = { 1, 2, 10 };
+
+        int maxIndex = 0;
+        int maxErtek = tomb[0];
+
+        for (int i = 1; i < n; i++) {
+            if (tomb[i] > maxErtek) {
+                maxIndex = i;
+                maxErtek = tomb[i];
+            }
+        }
+
+        Console.WriteLine($"{maxIndex}, {maxErtek}");
+        */
+
+        /*
+        // minimum kiválasztás
+        int n = 3;
+        int[] tomb = { 1, 2, 10 };
+
+        int minIndex = 0;
+        int minErtek = tomb[0];
+
+        for (int i = 1; i < n; i++) {
+            if (tomb[i] < minErtek) {
+                minIndex = i;
+                minErtek = tomb[i];
+            }
+        }
+
+        Console.WriteLine($"{minIndex}, {minErtek}");
+        */
+
+/*
+        // feltételes maximum kiválasztás
+        int n = 3;
+        int[] tomb = { 10, 8, 11 };
+
+        bool van = false;
+        int maxIndex;
+        int maxErtek;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (van && tomb[i] > 8)
+            {
+                if (tomb[i] > maxErtek)
+                {
+                    maxIndex = i;
+                    maxErtek = tomb[i];
+                }
+            }
+            else if (!van && tomb[i] > 8)
+            {
+                maxIndex = i;
+                maxErtek = tomb[i];
+                van = true;
+            }
+        }
+
+        Console.WriteLine($"{maxIndex}, {maxErtek}");
+        */
+    }
+}
