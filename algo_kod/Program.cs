3fcf35094857913cfba7008b7a0a2e251bad4c26
Réka Kupsza <rkupsza@diligent.com>
2025-01-14 20:34:59 +0100

    update

--- algo_kod/Program.cs
+++ algo_kod/Program.cs
@@ -1,9 +1,30 @@
 ﻿using System;
+
+/*
+// kiválogatás 2
+public struct Diak2
+{
+    public string Nev;
+    public int Jegy;
+
+    public Diak2(string Nev, int Jegy)
+    {
+        this.Nev = Nev;
+        this.Jegy = Jegy;
+    }
+}
+*/
+
 internal class Program
 {
     /*
     // kiválogatás
-      public record Diak(string Nev, int Jegy);
+    public record Diak(string Nev, int Jegy);
+    */
+
+    /*
+    // kiválogatás 2
+    public static Diak2[] diakok2;
     */
 
     static void Main(string[] args)
@@ -268,23 +289,47 @@ internal class Program
             Console.WriteLine(y[i]);
         }
 
-        // egy példa tömbre
-        // feltételezzük, hogy nem kaptunk n-et
-        int[] tomb = { 1, 20, 3, 40 };
-        int[] y2 = new int[tomb.Length];
+
+        int n2 = 3;
+        string[] y2 = new string[n];
+        diakok2 = new Diak2[] {
+            new Diak2("Balázs", 5),
+            new Diak2("Józsi", 3),
+            new Diak2("Géza", 5)
+        };
 
         int db2 = 0;
 
-        for (int i = 0; i < tomb.Length; i++) {
-            if (tomb[i] > 5) {
-                y2[db2] = tomb[i];
+        for (int i = 0; i < n; i++) {
+            if (diakok2[i].Jegy == 5) {
+                y2[db2] = diakok2[i].Nev;
                 db2++;
             }
         }
 
         Console.WriteLine(db2);
         for (int i = 0; i < db2; i++) {
-                Console.WriteLine(y2[i]);
+            Console.WriteLine(y2[i]);
+        }
+
+
+        // egy példa tömbre
+        // feltételezzük, hogy nem kaptunk n-et
+        int[] tomb = { 1, 20, 3, 40 };
+        int[] y3 = new int[tomb.Length];
+
+        int db3 = 0;
+
+        for (int i = 0; i < tomb.Length; i++) {
+            if (tomb[i] > 5) {
+                y3[db3] = tomb[i];
+                db3++;
+            }
+        }
+
+        Console.WriteLine(db3);
+        for (int i = 0; i < db3; i++) {
+            Console.WriteLine(y3[i]);
         }
         */
     }
